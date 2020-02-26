cd009c4b0e89cd464a02555691bca4724063c708
Sebastian Park <SebPark03@gmail.com>
2020-02-26 12:40:25 -0500

    Add special relativity with the bullets.
    The bullets now move faster in the direction of the ship when the ship moves faster.

--- AsteroidsThruster/src/Bullet.java
+++ AsteroidsThruster/src/Bullet.java
@@ -15,6 +15,23 @@ public class Bullet {
         xthrust = Math.cos(Math.toRadians(angle)) * velocity;
     }
 
+    public Bullet(int pXpos, int pYpos, double pAngle, double shipVelocity) {
+        xpos = pXpos;
+        ypos = pYpos;
+        angle = pAngle;
+        velocity = shipVelocity + velocity;
+        ythrust = Math.sin(Math.toRadians(angle)) * velocity;
+        xthrust = Math.cos(Math.toRadians(angle)) * velocity;
+    }
+
+    public Bullet(int pXpos, int pYpos, double pAngle, double shipdx, double shipdy) {
+        xpos = pXpos;
+        ypos = pYpos;
+        angle = pAngle;
+        ythrust = Math.sin(Math.toRadians(angle)) * velocity + shipdy;
+        xthrust = Math.cos(Math.toRadians(angle)) * velocity + shipdx;
+    }
+
     public void move(){
         ypos += ythrust;
         xpos += xthrust;
